71769e6c66bea529befb0592e2240ede28a95be2
inninform <157962471+inninform@users.noreply.github.com>
2024-01-28 16:00:33 +0100

    Add files via upload

---
+++ basic_calculator.py
@@ -0,0 +1,16 @@
+num1 = int(input("enter the first number  : "))
+op   = input("enter the operation     : ")
+num2 = int(input("enter the second number : "))
+if (op == "+"):
+	result = num1+num2
+	print(result)
+if (op == "-"):
+	result = num1-num2
+	print(result)
+if (op == "*"):
+	result = num1*num2
+	print(result)
+if (op == "/"):
+	result = num1/num2
+	print(result)
+ 
